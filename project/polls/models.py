963bd860b42d5dad79a97357b16f62177c023dd1
Vladimir Irkhin <ivb1989@yandex.ru>
2021-12-08 17:06:55 +0300

    Add field author in model Questions

--- project/polls/models.py
+++ project/polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
+    author = models.CharField(max_length=255, default='Ivanov Ivan')
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
